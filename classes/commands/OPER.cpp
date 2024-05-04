9765a6538d0beb8a915d5a82cade3f7a2848c8c4
Ynsbvv <youncoc@gmail.com>
2024-05-04 05:28:56 +0200

    set a map of operators

--- classes/commands/OPER.cpp
+++ classes/commands/OPER.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   OPER.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pibosc <pibosc@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ybelatar <ybelatar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 04:29:35 by ybelatar          #+#    #+#             */
-/*   Updated: 2024/05/04 04:57:05 by pibosc           ###   ########.fr       */
+/*   Updated: 2024/05/04 05:25:41 by ybelatar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,36 @@
 #include "Client.hpp"
 #include "Server.hpp"
 
-bool is_nick_valid(std::string nick) {
-	for (std::map<int, Client *>::iterator it = g_server.get_clients().begin(); it != g_server.get_clients().end(); it++) {
-		if ((*it).second->get_nick() == nick)
+bool is_operator_valid(std::string nick) {
+	for (std::map<std::string, std::string>::iterator it = g_server.get_operators().begin(); it != g_server.get_operators().end(); it++) {
+		if ((*it).first == nick)
 			return true;
 	}
 	return false;
 }
 
+// TODO irsii renvoie la commande une 2eme fois apres que le client soit devenu operator pk???
+
 
 int Client::command_OPER(t_command &cmd)
 {
-	if (!_has_password || !_has_Client || !_has_nick)
-	{
+	if (!_has_password || !_has_Client || !_has_nick) {
 		send_message(":ft_irc 451 * OPER :You have not registered");
-		return (0);
-	}
-	if (!is_nick_valid(cmd.parameters[0])) {
-		send_message(":ft_irc 501 * :No such user by that nickname");
 		return 0;
 	}
-	if (cmd.parameters[1] != "password")
-	{
+	if (_is_operator) {
+		send_message(":ft_irc 501 * :You are already an IRC operator");
+		return 0;
+	}
+	if (!is_operator_valid(cmd.parameters[0])) {
+		send_message(":ft_irc 501 * :No such operator by that name");
+		return 0;
+	}
+	if (cmd.parameters[1] != g_server.get_operators()[cmd.parameters[0]]) {
 		send_message(":ft_irc 464 * :Password incorrect");
 		return 0;
 	}
-	if (false) {
-		send_message(":ft_irc NOTICE " + cmd.parameters[0] + " :You are already an IRC operator");
-		return 0;
-	}
-	send_message("381 " + cmd.parameters[0] + " :You are now an IRC operator");
+	send_message(":ft_irc 381 " + cmd.parameters[0] + " :You are now an IRC operator");
+	_is_operator = true;
 	return 1;
 }
