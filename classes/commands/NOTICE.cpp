3f8b49dcc5221a10ceb547420b14487fe961d833
Pierre Bosc <pibosc@bess-f2r4s11.clusters.42paris.fr>
2024-05-04 04:55:36 +0200

    added NOTICE and PRIVMSG

--- classes/commands/NOTICE.cpp
+++ classes/commands/NOTICE.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   NOTICE.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybelatar <ybelatar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pibosc <pibosc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 04:29:33 by ybelatar          #+#    #+#             */
-/*   Updated: 2024/05/04 04:33:35 by ybelatar         ###   ########.fr       */
+/*   Updated: 2024/05/04 04:54:32 by pibosc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,49 @@
 
 int Client::command_NOTICE(t_command &command)
 {
-	(void)command;
-	send_message("ERROR :NOTICE command not implemented");
-	return 0;
+	Channel	*channel;
+
+	if (!_has_password || !_has_Client || !_has_nick)
+	{
+		send_message(":ft_irc 451 * NOTICE :You have not registered");
+		return (0);
+	}
+	if (command.parameters.size() < 1)
+	{
+		send_message(":ft_irc 461 * NOTICE :Not enough parameters");
+		return (0);
+	}
+	std::cout << command.command << ": " << command.parameters[0] << ": " << command.suffix << std::endl;
+	if (command.parameters[0].size() >= 2 && command.parameters[0][0] == '#')
+	{
+		if (g_server.get_channels().find(command.parameters[0]) != g_server.get_channels().end())
+			channel = g_server.get_channels()[command.parameters[0]];
+		else
+		{
+			send_message(":ft_irc 403 * " + command.parameters[0] + " :No such channel");
+			return (0);
+		}
+		if (!channel->is_user(_fd))
+		{
+			send_message(":ft_irc 442 * " + command.parameters[0] + " :You're not on that channel");
+			return (0);
+		}
+		std::string message = ":" + _nick + "!" + _user + "@" + "localhost" + " NOTICE " + command.parameters[0] + " :" + command.suffix;
+		channel->broadcast(message, _fd);
+		return (0);
+	}
+	for (std::map<int,
+		Client *>::iterator it = g_server.get_clients().begin(); it != g_server.get_clients().end(); it++)
+	{
+		if ((*it).second->get_nick() == command.parameters[0])
+		{
+			std::cout << "" << std::endl;
+			std::stringstream ss(command.suffix);
+			std::string first_word;
+			ss >> first_word;
+			(*it).second->send_message(":" + _nick + "!" + _user + "@" + "localhost" + " NOTICE " + command.parameters[0] + " :" + command.suffix + "\r\n");
+			return (0);
+		}
+	}
+	return (0);
 }
--- classes/commands/PRIVMSG.cpp
+++ classes/commands/PRIVMSG.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PRIVMSG.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybelatar <ybelatar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pibosc <pibosc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 04:29:46 by ybelatar          #+#    #+#             */
-/*   Updated: 2024/05/04 04:40:37 by ybelatar         ###   ########.fr       */
+/*   Updated: 2024/05/04 04:55:10 by pibosc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,43 +20,42 @@ int Client::command_PRIVMSG(t_command &command)
 
 	if (!_has_password || !_has_Client || !_has_nick)
 	{
-		send_message("ERROR :You must set your password, nick and user before sending a message");
+		send_message(":ft_irc 451 * PRIVMSG :You have not registered");
 		return (0);
 	}
 	if (command.parameters.size() < 1)
 	{
-		send_message("ERROR :Not enough parameters");
+		send_message(":ft_irc 411 * PRIVMSG :No recipient given");
 		return (0);
 	}
 	std::cout << command.command << ": " << command.parameters[0] << ": " << command.suffix << std::endl;
-	if (command.parameters.at(0).size() >= 2 && command.parameters.at(0).at(0) == '#')
+	if (command.parameters[0].size() >= 2 && command.parameters[0][0] == '#')
 	{
 		if (g_server.get_channels().find(command.parameters[0]) != g_server.get_channels().end())
 			channel = g_server.get_channels()[command.parameters[0]];
 		else
 		{
-			send_message("ERROR :No such channel " + command.parameters[0]);
+			send_message(":ft_irc 403 * " + command.parameters[0] + " :No such channel");
 			return (0);
 		}
 		if (!channel->is_user(_fd))
 		{
-			send_message("ERROR :You are not in channel "
-				+ command.parameters[0]);
+			send_message(":ft_irc 442 * " + command.parameters[0] + " :You're not on that channel");
 			return (0);
 		}
-		std::string message = ":" + _nick
-			+ " PRIVMSG " + command.parameters[0] + " :" + command.suffix;
+		std::string message = ":" + _nick + "!" + _user + "@" + "localhost" + " PRIVMSG " + command.parameters[0] + " :" + command.suffix;
 		channel->broadcast(message, _fd);
 		return (0);
 	}
-	for (std::map<int,
-		Client *>::iterator it = g_server.get_clients().begin(); it != g_server.get_clients().end(); it++)
+	for (std::map<int, Client *>::iterator it = g_server.get_clients().begin(); it != g_server.get_clients().end(); it++)
 	{
 		if ((*it).second->get_nick() == command.parameters[0])
 		{
 			std::cout << "" << std::endl;
-			(*it).second->send_message(":" + _nick
-				+ " PRIVMSG " + command.parameters[0] + " :" + command.suffix);
+			std::stringstream ss(command.suffix);
+			std::string first_word;
+			ss >> first_word;
+			(*it).second->send_message(":" + _nick + "!" + _user + "@" + "localhost" + " PRIVMSG " + command.parameters[0] + " :" + command.suffix + "\r\n");
 			return (0);
 		}
 	}
