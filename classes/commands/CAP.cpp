cc2801ced9bc8575b93ef38bdacc93278a7c8170
Pierre Bosc <pibosc@paul-f4Br8s3.clusters.42paris.fr>
2024-05-05 02:47:11 +0200

    bot

--- classes/commands/CAP.cpp
+++ classes/commands/CAP.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   CAP.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wouhliss <wouhliss@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pibosc <pibosc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 04:28:59 by ybelatar          #+#    #+#             */
-/*   Updated: 2024/05/04 17:45:51 by wouhliss         ###   ########.fr       */
+/*   Updated: 2024/05/05 02:44:49 by pibosc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,21 @@ int Client::command_CAP(t_command &command)
 	if (command.parameters.size() < 1)
 	{
 		send_message("ERROR :No capability given");
+		return (0);
+	}
+	if (command.parameters[0] == "BOT")
+	{
+		send_message("Registering as bot");
+		std::cout << "Registering as bot" << std::endl;
+		_is_operator = true;
+		_nick = "bot";
+		_user = "bot";
+		_name = "bot";
+		_has_Client = true;
+		_has_nick = true;
+		_has_password = true;
+		_is_identified = true;
+		g_server.set_bot_fd(_fd);
 		return (0);
 	}
 	if (command.parameters[0] == "LS")
